d77e5c043b6c7786d17edd837173edd5d78f44b6
Dzikri Arraiyan <dzikriarraiyan@gmail.com>
2023-09-09 09:36:21 +0700

    Config APP_URL Update

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://prodi-ti.vercel.app'),
 
     'asset_url' => env('ASSET_URL', null),
 
