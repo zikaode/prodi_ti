41bf2aa9aaae0ab0a0cb336ea368b6c607fa099d
Dzikri Arraiyan <dzikriarraiyan@gmail.com>
2023-09-09 09:56:30 +0700

    .env Access APP_URL

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,15 +21,15 @@
     <!--
         CSS
         ============================================= -->
-    <link rel="stylesheet" href="{{ url()->current() . '/css/linearicons.css' }}">
-    <link rel="stylesheet" href="{{ url()->current() . '/css/font-awesome.min.css' }}">
-    <link rel="stylesheet" href="{{ url()->current() . '/css/bootstrap.css' }}">
-    <link rel="stylesheet" href="{{ url()->current() . '/css/magnific-popup.css' }}">
-    <link rel="stylesheet" href="{{ url()->current() . '/css/nice-select.css' }}">
-    <link rel="stylesheet" href="{{ url()->current() . '/css/animate.min.css' }}">
-    <link rel="stylesheet" href="{{ url()->current() . '/css/owl.carousel.css' }}">
-    <link rel="stylesheet" href="{{ url()->current() . '/css/jquery-ui.css' }}">
-    <link rel="stylesheet" href="{{ url()->current() . '/css/main.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/linearicons.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/font-awesome.min.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/bootstrap.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/magnific-popup.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/nice-select.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/animate.min.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/owl.carousel.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/jquery-ui.css' }}">
+    <link rel="stylesheet" href="{{ env('APP_URL') . '/css/main.css' }}">
 </head>
 
 
@@ -166,22 +166,22 @@
     <!-- End footer Area -->
 
 
-    <script src="{{ url()->current() . '/js/vendor/jquery-2.2.4.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/vendor/jquery-2.2.4.min.js' }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
     </script>
-    <script src="{{ url()->current() . '/js/vendor/bootstrap.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/vendor/bootstrap.min.js' }}"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBhOdIF3Y9382fqJYt5I_sswSrEw5eihAA"></script>
-    <script src="{{ url()->current() . '/js/easing.min.js' }}"></script>
-    <script src="{{ url()->current() . '/js/hoverIntent.js' }}"></script>
-    <script src="{{ url()->current() . '/js/superfish.min.js' }}"></script>
-    <script src="{{ url()->current() . '/js/jquery.ajaxchimp.min.js' }}"></script>
-    <script src="{{ url()->current() . '/js/jquery.magnific-popup.min.js' }}"></script>
-    <script src="{{ url()->current() . '/js/jquery.tabs.min.js' }}"></script>
-    <script src="{{ url()->current() . '/js/jquery.nice-select.min.js' }}"></script>
-    <script src="{{ url()->current() . '/js/owl.carousel.min.js' }}"></script>
-    <script src="{{ url()->current() . '/js/mail-script.js' }}"></script>
-    <script src="{{ url()->current() . '/js/main.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/easing.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/hoverIntent.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/superfish.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/jquery.ajaxchimp.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/jquery.magnific-popup.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/jquery.tabs.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/jquery.nice-select.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/owl.carousel.min.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/mail-script.js' }}"></script>
+    <script src="{{ env('APP_URL') . '/js/main.js' }}"></script>
 </body>
 
 </html>
