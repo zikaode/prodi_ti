7fdb555ee4e12e89b8e894b1c0d1148a0611d22e
fauzul2002 <fauzulshalis5@gmail.com>
2023-11-13 12:49:17 +0700

    commit

--- app/Http/Controllers/staticController.php
+++ app/Http/Controllers/staticController.php
@@ -49,62 +49,6 @@ class staticController extends Controller
     {
         return view('laboratorium');
     }
-    public function Berita()
-    {
-        return view('Berita');
-    }
-}
-<?php
-
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class staticController extends Controller
-{
-    /**
-     * Display a Index Of Web.
-     */
-
-    public function index()
-    {
-        return view('home');
-    }
-
-    public function profil()
-    {
-        return view('profil');
-    }
-
-    public function dataMahasiswa()
-    {
-        return view('dataMahasiswa');
-    }
-
-    public function strukturPimpinan()
-    {
-        return view('strukturPimpinan');
-    }
-
-    public function kuesionerMahasiswa()
-    {
-        return view('kuesionerMahasiswa');
-    }
-
-    public function kuesionerAlumni()
-    {
-        return view('kuesionerAlumni');
-    }
-
-    public function dataDosen()
-    {
-        return view('dataDosen');
-    }
-
-    public function laboratorium()
-    {
-        return view('laboratorium');
-    }
 
     public function pages($param)
     {
