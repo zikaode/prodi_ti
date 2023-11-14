f3dab27d76b415eda7cbe1324eadc8b004b43373
Dzikri Arraiyan <dzikriarraiyan@gmail.com>
2023-11-14 10:41:52 +0700

    Commit

--- resources/views/general.blade.php
+++ resources/views/general.blade.php
@@ -8,10 +8,10 @@
             <div class="row d-flex align-items-center justify-content-center">
                 <div class="about-content col-lg-12">
                     <h1 class="text-white text-uppercase">
-                        UMUM
+                        umum
                     </h1>
                     <p class="text-white link-nav"><a href="{{ route('static.index') }}">Home </a> <span
-                            class="lnr lnr-arrow-right"></span><a href="{{ route('static.general') }}">Umum</a>
+                            class="lnr lnr-arrow-right"></span><a href="{{ route('static.general') }}">umum</a>
                     </p>
                 </div>
             </div>
@@ -26,7 +26,7 @@
                 <div class="col-lg-12 posts-list">
                     <div class="single-post row">
                         <div class="col-lg-12 col-md-12 text-center">
-                            <h3 class="mt-20 mb-20" style="color: black">FASILITAS UMUM PROGRAM STUDI TEKNIK INFORMATIKA
+                            <h3 class="mt-20 mb-20" style="color: black">FASILITAS umum PROGRAM STUDI TEKNIK INFORMATIKA
                             </h3>
                         </div>
                         <div class="col-lg-12 mb-4 widget-image">
@@ -42,7 +42,7 @@
                     <div class="col-lg-3 col-md-6 d-flex justify-content-center align-items-center mb-3"
                         style="box-shadow: 100px">
                         <div class="card mx-1" style="width: 18rem; height: 14rem;">
-                            <img src="{{ env('APP_URL') . '/image/Umum/masjid.jpg' }}" class="card-img-top" alt="..."
+                            <img src="{{ env('APP_URL') . '/image/umum/masjid.jpg' }}" class="card-img-top" alt="..."
                                 style="height: 10rem">
                             <div class="card-body">
                                 <h5 class="card-title text-center"><a href="#" class="text-black">MASJID AL-BAYYAN</a>
@@ -53,7 +53,7 @@
                     <div class="col-lg-3 col-md-6 d-flex justify-content-center align-items-center"
                         style="box-shadow: 100px">
                         <div class="card mx-1" style="width: 18rem; height: 15rem;">
-                            <img src="{{ env('APP_URL') . '/image/Umum/kantin.jpg' }}" class="card-img-top" alt="..."
+                            <img src="{{ env('APP_URL') . '/image/umum/kantin.jpg' }}" class="card-img-top" alt="..."
                                 style="height: 10rem">
                             <div class="card-body">
                                 <h5 class="card-title text-center"><a href="#" class="text-black">KANTIN</a></h5>
@@ -63,7 +63,7 @@
                     <div class="col-lg-3 col-md-6 d-flex justify-content-center align-items-center"
                         style="box-shadow: 100px">
                         <div class="card mx-1" style="width: 18rem; height: 15rem;">
-                            <img src="{{ env('APP_URL') . '/image/Umum/parkir.jpg' }}" class="card-img-top" alt="..."
+                            <img src="{{ env('APP_URL') . '/image/umum/parkir.jpg' }}" class="card-img-top" alt="..."
                                 style="height: 10rem">
                             <div class="card-body">
                                 <h5 class="card-title text-center"><a href="#" class="text-black">PARKIR </a></h5>
@@ -73,7 +73,7 @@
                     <div class="col-lg-3 col-md-6 d-flex justify-content-center align-items-center"
                         style="box-shadow: 100px">
                         <div class="card mx-1" style="width: 18rem; height: 15rem;">
-                            <img src="{{ env('APP_URL') . '/image/Umum/taman.jpeg' }}" class="card-img-top" alt="..."
+                            <img src="{{ env('APP_URL') . '/image/umum/taman.jpeg' }}" class="card-img-top" alt="..."
                                 style="height: 10rem">
                             <div class="card-body">
                                 <h5 class="card-title text-center"><a href="#" class="text-black">TAMAN </a></h5>
